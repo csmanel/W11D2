4ecff217a252a596dc30070f6c4e75cd172ef785
Albert Dedushi <adedushi@hotmail.com>
2024-01-02 14:25:39 -0500

    Complete phase 3

--- aa14-react-context-greenhouse/src/main.jsx
+++ aa14-react-context-greenhouse/src/main.jsx
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import ThemeProvider from './context/ThemeContext';
+import ClimateProvider from './context/ClimateContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ClimateProvider>
       <ThemeProvider>
-        <App />
+        
+          <App />
+        
       </ThemeProvider>
+    </ClimateProvider>
   </React.StrictMode>
 );
